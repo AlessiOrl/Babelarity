aae497f2f839a6a9245bcebfd169b167df8e6fe2
Alessio Orlando <38555384+AlessiOrl@users.noreply.github.com>
2018-10-07 15:15:39 +0200

    Changed implementation of VectorizedWords

--- src/it/uniroma1/lcl/babelarity/VectorizedWords.java
+++ src/it/uniroma1/lcl/babelarity/VectorizedWords.java
@@ -5,17 +5,16 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public class VectorizedWords implements Map<String, Float[]>
+public class VectorizedWords extends HashMap<String, Float[]>
 {
 
     private static int MIN_VALUE = 2;
-    private HashMap<String, Float[]> vectorizedWords;
     private int maxLength;
 
     public VectorizedWords(int maxLength)
     {
-        vectorizedWords = new HashMap<>();
-        this.maxLength = maxLength > 2 ? maxLength : MIN_VALUE;
+        super();
+        this.maxLength = maxLength > MIN_VALUE ? maxLength : MIN_VALUE;
     }
 
     public VectorizedWords()
@@ -23,81 +22,16 @@ public class VectorizedWords implements Map<String, Float[]>
         this(200);
     }
 
-    @Override
-    public int size()
-    {
-        return vectorizedWords.size();
-    }
-
-    @Override
-    public boolean isEmpty()
-    {
-        return vectorizedWords.size() == 0;
-    }
-
-    @Override
-    public boolean containsKey(Object key)
-    {
-        return vectorizedWords.containsKey(key);
-    }
-
-    @Override
-    public boolean containsValue(Object value)
-    {
-        return vectorizedWords.containsValue(value);
-    }
-
-    @Override
-    public Float[] get(Object key)
-    {
-        return vectorizedWords.get(key);
-    }
-
     public Float[] put(String word, Float[] vector)
     {
-        if (vectorizedWords.size() <= maxLength) return vectorizedWords.put(word, vector);
+        if (this.size() <= maxLength) return super.put(word, vector);
         this.remove();
-        return this.put(word, vector);
+        return super.put(word, vector);
     }
 
-    @Override
-    public Float[] remove(Object key)
+    private Float[] remove()
     {
-        return vectorizedWords.remove(key);
+        return super.remove(this.keySet().iterator().next());
     }
 
-    public Float[] remove()
-    {
-        return vectorizedWords.remove(vectorizedWords.keySet().iterator().next());
-    }
-
-    @Override
-    public void putAll(Map<? extends String, ? extends Float[]> m)
-    {
-        vectorizedWords.putAll(m);
-    }
-
-    @Override
-    public void clear()
-    {
-        vectorizedWords = new HashMap<>();
-    }
-
-    @Override
-    public Set<String> keySet()
-    {
-        return vectorizedWords.keySet();
-    }
-
-    @Override
-    public Collection<Float[]> values()
-    {
-        return vectorizedWords.values();
-    }
-
-    @Override
-    public Set<Entry<String, Float[]>> entrySet()
-    {
-        return vectorizedWords.entrySet();
-    }
 }
